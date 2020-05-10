85b94b46bad1b2a90a83ba927a2cb896891dab58
mfright <uj020288@yahoo.co.jp>
2020-05-10 15:32:57 +0900

    ver1.0

---
+++ colorEditor2/colorEditor2/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace colorEditor2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSelectInputFile = new System.Windows.Forms.Button();
+            this.txtInputFilePath = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtOutputFilePath = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.btnStartEdit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSelectInputFile
+            // 
+            this.btnSelectInputFile.Location = new System.Drawing.Point(636, 16);
+            this.btnSelectInputFile.Name = "btnSelectInputFile";
+            this.btnSelectInputFile.Size = new System.Drawing.Size(129, 23);
+            this.btnSelectInputFile.TabIndex = 0;
+            this.btnSelectInputFile.Text = "select";
+            this.btnSelectInputFile.UseVisualStyleBackColor = true;
+            this.btnSelectInputFile.Click += new System.EventHandler(this.btnSelectInputFile_Click);
+            // 
+            // txtInputFilePath
+            // 
+            this.txtInputFilePath.Location = new System.Drawing.Point(16, 18);
+            this.txtInputFilePath.Name = "txtInputFilePath";
+            this.txtInputFilePath.Size = new System.Drawing.Size(614, 19);
+            this.txtInputFilePath.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtInputFilePath);
+            this.groupBox1.Controls.Add(this.btnSelectInputFile);
+            this.groupBox1.Location = new System.Drawing.Point(17, 85);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(771, 49);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "1. Select an image file to edit. (input)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtOutputFilePath);
+            this.groupBox2.Location = new System.Drawing.Point(17, 140);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(771, 57);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "2. Set output filename below. (output)";
+            // 
+            // txtOutputFilePath
+            // 
+            this.txtOutputFilePath.Location = new System.Drawing.Point(16, 27);
+            this.txtOutputFilePath.Name = "txtOutputFilePath";
+            this.txtOutputFilePath.Size = new System.Drawing.Size(614, 19);
+            this.txtOutputFilePath.TabIndex = 5;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.lblDescription.Location = new System.Drawing.Point(15, 9);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(249, 48);
+            this.lblDescription.TabIndex = 4;
+            this.lblDescription.Text = "1. Select an image file to edit below.\r\n2. Set output filename below.\r\n3. Press E" +
+    "DIT button.";
+            // 
+            // btnStartEdit
+            // 
+            this.btnStartEdit.Location = new System.Drawing.Point(436, 203);
+            this.btnStartEdit.Name = "btnStartEdit";
+            this.btnStartEdit.Size = new System.Drawing.Size(352, 58);
+            this.btnStartEdit.TabIndex = 5;
+            this.btnStartEdit.Text = "3. EDIT";
+            this.btnStartEdit.UseVisualStyleBackColor = true;
+            this.btnStartEdit.Click += new System.EventHandler(this.btnStartEdit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 276);
+            this.Controls.Add(this.btnStartEdit);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AutumnEditor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectInputFile;
+        private System.Windows.Forms.TextBox txtInputFilePath;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtOutputFilePath;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Button btnStartEdit;
+    }
+}
+
---
+++ colorEditor2/colorEditor2/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace colorEditor2
+{
+    public partial class Form1 : Form
+    {
+        
+        // こんすとらくた
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 画像ファイル選択ボタンが押されたとき
+        private void btnSelectInputFile_Click(object sender, EventArgs e)
+        {
+            txtInputFilePath.Text = ImageFileSelect();
+
+            if (txtInputFilePath.Text.Length > 0)
+            {
+                int ind = txtInputFilePath.Text.LastIndexOf(".");
+
+                txtOutputFilePath.Text = txtInputFilePath.Text.Substring(0, ind) + "_output.jpg";
+            }
+
+            
+        }
+
+
+
+        //ファイル選択ダイアログ表示
+        private String ImageFileSelect()
+        {
+            //ファイルを開くダイアログボックスの作成  
+            var ofd = new OpenFileDialog();
+            //ファイルフィルタ  
+            ofd.Filter = "Image File(*.bmp,*.jpg,*.png,*.tif)|*.bmp;*.jpg;*.png;*.tif|Bitmap(*.bmp)|*.bmp|Jpeg(*.jpg)|*.jpg|PNG(*.png)|*.png";
+            //ダイアログの表示 （Cancelボタンがクリックされた場合は何もしない）
+            if (ofd.ShowDialog() == DialogResult.Cancel) return null;
+
+            return ofd.FileName;
+        }
+
+
+
+        // 指定されたファイルのBitmapをロードして返す。
+        private Bitmap ImageFileOpen(string filePath)
+        {
+            // 指定したファイルが存在するか？確認
+            if (System.IO.File.Exists(filePath) == false) return null;
+
+            // 拡張子の確認
+            var ext = System.IO.Path.GetExtension(filePath).ToLower();
+
+            // ファイルの拡張子が対応しているファイルかどうか調べる
+            if (
+                (ext != ".bmp") &&
+                (ext != ".jpg") &&
+                (ext != ".png") &&
+                (ext != ".tif")
+                )
+            {
+                return null;
+            }
+
+            Bitmap bmp;
+
+            // ファイルストリームでファイルを開く
+            using (var fs = new System.IO.FileStream(
+                filePath,
+                System.IO.FileMode.Open,
+                System.IO.FileAccess.Read))
+            {
+                bmp = new Bitmap(fs);
+            }
+            return bmp;
+        }
+
+
+
+        //Formに文字を別スレッドから表示させるデリゲート
+        delegate void SetTextCallback(string text);
+
+        //Formに文字を別スレッドから表示させるメソッド
+        private void SetText(string text)
+        {
+            // コントロールが破棄されているならば、何もしない
+            if (lblDescription.IsDisposed) return;
+
+            // 呼び出し元のコントロールのスレッドと異なるかを確認する
+            if (lblDescription.InvokeRequired)
+            {
+                // このブロックは、別スレッドで実行される
+
+                // 同一メソッドへのコールバックを作成する
+                SetTextCallback delegateMethod = new SetTextCallback(SetText);
+
+                // コントロールのInvoke()メソッドを呼び出すことで、コントロールのスレッドでこのメソッドを実行する
+                lblDescription.Invoke(delegateMethod, new object[] { text });
+            }
+            else
+            {
+                // このブロックは、同一スレッドで実行される
+
+                // コントロールを直接呼び出す
+                lblDescription.Text = text;
+            }
+        }
+
+
+
+        // 編集開始ボタン押下したとき
+        private void btnStartEdit_Click(object sender, EventArgs e)
+        {
+            if (txtInputFilePath.Text.Length == 0)
+            {
+                return;
+            }
+
+                // 別スレッドで動作
+                System.Threading.Thread thread = new Thread(new ThreadStart(() =>
+            {
+
+                SetText("started.");
+
+
+                Bitmap bmp = ImageFileOpen(txtInputFilePath.Text);
+
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    
+                    SetText("Converting... " + x + "/" + bmp.Width);
+                    
+
+                    for (int y = 0; y < bmp.Height; y++)
+                    {
+                        Color color1 = bmp.GetPixel(x, y);
+
+                        int redColor = color1.R;
+                        int greenColor = color1.G;
+                        int blueColor = color1.B;
+
+                        if (color1.R < color1.G)
+                        {
+                            //赤よりも緑が強いとき、入れ替え
+
+
+                            redColor = color1.G;
+
+                            greenColor = color1.R;
+
+                            if (greenColor > 30)
+                            {
+                                greenColor -= 30;
+                            }
+
+                            if (redColor < 220)
+                            {
+                                redColor += 30;
+                            }
+
+
+                        }
+
+
+
+                        Color newColor = Color.FromArgb(redColor, greenColor, blueColor);
+
+                        bmp.SetPixel(x, y, newColor);
+
+                        color1 = newColor;
+
+                    }
+                }
+
+                //pictureBox1.Image = bmp;
+
+                SetText("Finished.");
+
+                bmp.Save(txtOutputFilePath.Text, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                Process process = new Process();
+                ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe", "/c " + txtOutputFilePath.Text);
+                processStartInfo.CreateNoWindow = true;
+                processStartInfo.UseShellExecute = false;
+
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.RedirectStandardError = true;
+
+                process = Process.Start(processStartInfo);
+                process.WaitForExit();
+
+            }));
+
+            // スレッド起動
+            thread.Start();
+
+        }
+    }
+}
